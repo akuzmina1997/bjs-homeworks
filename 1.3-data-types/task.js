52d6fd50e0eda2d72a7eb2acc83b85952e0031af
Anastasiya Kuzmina <55270579+akuzmina1997@users.noreply.github.com>
2020-09-01 01:44:02 +0300

    first commit wrappers-for-primitives

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,32 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
     // код для задачи №1 писать здесь
-    // return totalAmount;
+    if (!Number(percent))  {
+        return("Параметр \"Процентная ставка\" содержит неправильное значение " + percent);
+    }
+
+    if (!Number(amount)) {
+        return(`Параметр "Сумма кредита" содержит неправильное значение ${amount}`);
+    } 
+
+    if (!Number(contribution) && Number(contribution) != 0) {
+        return(`Параметр "Сумма первоначального взноса" содержит неправильное значение ${contribution}`);
+    }
+
+    let totalMonths = (date.getFullYear() - new Date().getFullYear()) * 12 + (date.getMonth() - new Date().getMonth());
+    let creditBody = amount - contribution;
+    let monthlyPayment = creditBody * (((percent / 100) / 12) + ((percent / 100) / 12) / (((1 + ((percent / 100) / 12)) ** totalMonths) - 1));
+    let totalAmount = + (totalMonths * monthlyPayment).toFixed(2);
+
+    return totalAmount; // return totalAmount;
 }
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
-    // return greeting;
+    if (name === null
+        ||  !name ) {
+        name = 'Аноним';
+    }
+
+    let greeting = `Привет, мир! Меня зовут ${name}`;
+    return greeting;// return greeting;
 }
